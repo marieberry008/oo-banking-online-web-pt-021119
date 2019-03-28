fed9e333ec724f2fb70a5ce364a2f9d08dd6655d
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-28 21:43:21 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
 class Transfer
-  # your code here
+attr_accessor :sender, :receiver, :amount, :status
+
+def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    
+end
+
+def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+    # alternatively:
+    # if sender.valid? && receiver.valid?
+      # true
+    # else
+      # false
+    # end
+end
+
+def execute_transaction
+    if @sender.balance > @transfer_amount && @status == "pending"
+      @sender.balance -= @transfer_amount
+      @receiver.balance += @transfer_amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+end
+
+def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @transfer_amount
+      @receiver.balance -= @transfer_amount
+      @status = "reversed"
+    end
+end
+  
 end
